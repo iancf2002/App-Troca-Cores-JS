b330e267c0ec38f9622b7622bd87e4591bb8295a
iancf2002 <93164795+iancf2002@users.noreply.github.com>
2024-08-16 14:11:32 -0300

    Update index.js
    adicionando explicações as funções

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const body = document.getElementsByTagName("body")[0]
 
+// função que vai mudar a cor de acordo com a tag referenciada(name)
 function setColor(name) {
     body.style.backgroundColor = name;
 
 }
+//função da cor aleatória ao clicar no botão Aleatório.
 function randomColor() {
     const yellow= Math.round(Math.random() * 255)
     const green = Math.round(Math.random() * 255)
